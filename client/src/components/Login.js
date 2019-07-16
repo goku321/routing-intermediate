40fdd1ba750797bebb3b4c148cf84eb318f0aa51
Deepak <sah.sslpu@gmail.com>
2019-07-16 23:40:53 +0530

    Added redirect logic

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -18,6 +18,14 @@ class Login extends Component {
     ));
   };
 
+  redirectPath = () => {
+    const locationState = this.props.location.state;
+    const pathname = (
+      locationState && locationState.from && locationState.from.pathname
+    );
+    return pathname || '/albums';
+  };
+
   render() {
     if (this.state.shouldRedirect) {
       return (
